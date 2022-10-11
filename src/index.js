127b87ca3cfe1b0ddc0b5c8e1d7336bdb2e44798
Cililocwee <corrie.stroup@gmail.com>
2022-10-11 13:13:01 +0700

    basic structure of the html and interactivity outline with js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import css from "./style.css"
+
+const dropdownheaders = document.querySelectorAll('.dropdownheader');
+
+for (let i = 0; i < dropdownheaders.length; i++) {
+  dropdownheaders[i].addEventListener('click', () =>{
+    dropdownheaders[i].nextElementSibling.classList.toggle('hidden');
+  })
+}
+
+
+
+
+const aboutcut = document.querySelector('.aboutcut');
+aboutcut.onclick = document.getElementById("about").scrollIntoView;
