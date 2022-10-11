ffd95205ed3ccd0399e6744e966667813a80645a
Cililocwee <corrie.stroup@gmail.com>
2022-10-11 16:41:04 +0700

    added skills projects and contact section, added functionality to the navigation buttons

--- src/index.js
+++ src/index.js
@@ -12,7 +12,24 @@ for (let i = 0; i < dropdownheaders.length; i++) {
 
 
 const aboutcut = document.querySelector(".aboutcut");
-aboutcut.onclick = document.getElementById("about").scrollIntoView;
+aboutcut.addEventListener('click', () =>{
+    document.getElementById("about").scrollIntoView();
+})
+
+const skillscut = document.querySelector(".skillscut");
+skillscut.addEventListener('click', () =>{
+    document.getElementById("skills").scrollIntoView();
+})
+
+const projectscut = document.querySelector(".projectscut");
+projectscut.addEventListener('click', () =>{
+    document.getElementById("projects").scrollIntoView();
+})
+
+const contactcut = document.querySelector(".contactcut");
+contactcut.addEventListener('click', () =>{
+    document.getElementById("contact").scrollIntoView();
+})
 
 const returntotop = document.querySelector(".returntotop");
 returntotop.addEventListener('click', () =>{
