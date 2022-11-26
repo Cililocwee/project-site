604f4267cb655a356374ce7643b00eeb15eafb8d
Cililocwee <corrie.stroup@gmail.com>
2022-11-26 22:41:20 +0700

    fixed icon issue #1

--- src/skills-section/skillSetup.js
+++ src/skills-section/skillSetup.js
@@ -1,67 +1,68 @@
 import skillCard from "./skillCard";
+import jsLogo from "./icon-assets/javascript.svg";
 
 export default function setupSkills() {
   // ** Skill cards
   // src alt label
 
   const javascript = skillCard.cardFactory(
-    "/dist/icon-assets/javascript.svg",
+    jsLogo,
     "javascript logo",
     "Javascript"
   ).card;
   const reactjs = skillCard.cardFactory(
-    "/dist/icon-assets/reactjs.svg",
+    "./icon-assets/reactjs.svg",
     "reactjs logo",
     "React"
   ).card;
   const html = skillCard.cardFactory(
-    "/dist/icon-assets/HTML5.svg",
+    "./icon-assets/HTML5.svg",
     "HTML5 logo",
     "HTML"
   ).card;
   const css = skillCard.cardFactory(
-    "/dist/icon-assets/css.svg",
+    "./icon-assets/css.svg",
     "CSS3 logo",
     "CSS"
   ).card;
   const webpack = skillCard.cardFactory(
-    "/dist/icon-assets/webpack.svg",
+    "./icon-assets/webpack.svg",
     "webpack logo",
     "Webpack"
   ).card;
   const nodejs = skillCard.cardFactory(
-    "/dist/icon-assets/node-js.svg",
+    "./icon-assets/node-js.svg",
     "NodeJS logo",
     "NodeJS"
   ).card;
   const typescript = skillCard.cardFactory(
-    "/dist/icon-assets/typescript.svg",
+    "./icon-assets/typescript.svg",
     "typescript logo",
     "Typescript"
   ).card;
   const python = skillCard.cardFactory(
-    "/dist/icon-assets/python.svg",
+    "./icon-assets/python.svg",
     "python logo",
     "Python"
   ).card;
   const django = skillCard.cardFactory(
-    "/dist/icon-assets/django.svg",
+    "./icon-assets/django.svg",
     "django logo",
     "Django"
   ).card;
   const java = skillCard.cardFactory(
-    "/dist/icon-assets/java.svg",
+    "./icon-assets/java.svg",
     "java logo",
     "Java"
   ).card;
   const vue = skillCard.cardFactory(
-    "/dist/icon-assets/vue-9.svg",
+    "./icon-assets/vue-9.svg",
     "VueJS logo",
     "Vue"
   ).card;
 
   const java2 = skillCard.cardFactory(
-    "/dist/icon-assets/java.svg",
+    "./icon-assets/java.svg",
     "java logo",
     "Java"
   ).card;
